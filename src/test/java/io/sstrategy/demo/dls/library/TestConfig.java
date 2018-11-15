43808fde6890dccd8ebc528559e3001a4481ba03
Alberto Santos Arenillas <alberto.santos@sstrategy.io>
2018-11-15 09:58:03 +0100

    Custom data source in library tests.

--- src/test/java/io/sstrategy/demo/dls/library/TestConfig.java
+++ src/test/java/io/sstrategy/demo/dls/library/TestConfig.java
@@ -1,7 +1,12 @@
 package io.sstrategy.demo.dls.library;
 
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 /*
@@ -16,5 +21,35 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = "io.sstrategy")
 public class TestConfig {
 
+	@Value("${connection.driver}")
+	private String connectionDriver;
+	
+	@Value("${connection.url}")
+	private String connectionUrl;
 
+	@Value("${connection.username}")
+	private String connectionUsername;
+	
+	@Value("${connection.password}")
+	private String connectionPassword;
+	
+	
+	/**
+	 * Define el Data Source a usar en los tests. 
+	 * 
+	 * Esta definición de un Data Source no es necesaria si se quiere dejar que Spring utilice su Data Source por defecto
+	 * en los tests anotados con @SpringBootTest. El Data Source por defecto es una base de datos m2. Se incluye
+	 * en este proyecto para demostrar cómo sobreescribir ese Data Source por defecto.
+	 */
+	@Bean
+    public DataSource dataSource() {
+    	
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(connectionDriver);
+        dataSource.setUrl(connectionUrl);
+        dataSource.setUsername(connectionUsername);
+        dataSource.setPassword(connectionPassword);
+        
+        return dataSource;
+    }
 }
--- src/test/java/io/sstrategy/demo/dls/library/TestDatabaseService.java
+++ src/test/java/io/sstrategy/demo/dls/library/TestDatabaseService.java
@@ -6,9 +6,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import io.sstrategy.demo.dls.library.model.Animal;
@@ -18,8 +16,7 @@ import io.sstrategy.demo.dls.library.model.repo.AnimalRepo;
  * This is an integration test that demonstrates how to test a service that interacts with a database.
  */
 @RunWith(SpringRunner.class)
-@DataJpaTest
-@AutoConfigureTestDatabase(replace=Replace.NONE)
+@SpringBootTest
 public class TestDatabaseService {
 	
 	@Autowired
